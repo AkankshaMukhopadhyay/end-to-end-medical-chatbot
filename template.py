7ec6a1d233e46f066a992c4d5cede5801374220f
Akanksha Mukhopadhyay <akanksha.sutapa@gmail.com>
2025-10-20 13:55:49 +0530

    created files

--- template.py
+++ template.py
@@ -11,7 +11,7 @@ list_of_files=[
     "research/trials.ipynb",
     "app.py",
     "store_index.py",
-    "static",
+    "static/.gitkeep",
     "templates/chat.html"
 ]
 
@@ -19,9 +19,9 @@ for filepath in list_of_files:
     filepath=Path(filepath)
     filedir,filename=os.path.split(filepath)
     if filedir!="":
-        os.makedir(filedir,exist_ok=True)
+        os.makedirs(filedir,exist_ok=True)
         logging.info(f"creating directory;{filedir} for the file{filename}")
-    if (not os.path.exists(filepath)) or (os.path.getsize(filename)==0):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
         with open(filepath,'w') as f:
             pass
             logging.info(f"creating empty file:{filepath}")
